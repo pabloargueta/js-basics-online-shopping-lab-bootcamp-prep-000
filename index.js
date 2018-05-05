90a84a68d355d101c83779d88eccc28b6b307cdc
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-05 18:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = math.floor((math.random()*100)+1);
+ var price = Math.floor((Math.random()*100)+1);
  var itemObject = {itemName: item, itemPrice: price};
  cart.push(itemObject);
  return `${item} has been added to your cart`;
