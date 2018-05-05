0f01edc6f685f882f0904cb5a776af352bddd5c5
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-05 17:52:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if(cart[0] === undefined) {
+    return "Your shopping cart is empty";
+  }
 }
 
 function total() {
